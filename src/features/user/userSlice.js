59224a4b98218b361fb61e2f66d1f08fca0580cb
0rph3us <64805107+shadythegiant@users.noreply.github.com>
2025-04-05 13:57:48 +0000

    Thunks is back again :octocat:

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,28 +1,48 @@
-import { createSlice } from "@reduxjs/toolkit";
-// function getPosition() {
-//   return new Promise(function (resolve, reject) {
-//     navigator.geolocation.getCurrentPosition(resolve, reject);
-//   });
-// }
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import  {getAddress} from "../../services/apiGeocoding"; 
+import { build } from "vite";
+//
 
-// async function fetchAddress() {
-//   // 1) We get the user's geolocation position
-//   const positionObj = await getPosition();
-//   const position = {
-//     latitude: positionObj.coords.latitude,
-//     longitude: positionObj.coords.longitude,
-//   };
 
-//   // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
-//   const addressObj = await getAddress(position);
-//   const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+function getPosition() {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
 
-//   // 3) Then we return an object with the data that we are interested in
-//   return { position, address };
-// }
+
+
+
+// using the mf middleware! 
+
+// this fetch adress is going to be the action creator function called in our code
+
+// the createAsyncThunk creates 3 additional action types ( fullfiled / rejected  / pending / )
+
+export const fetchAddress = createAsyncThunk('user/fetchAddress' , async function() {
+  const positionObj = await getPosition();
+  const position = {
+    latitude: positionObj.coords.latitude,
+    longitude: positionObj.coords.longitude,
+  };
+
+  // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+  const addressObj = await getAddress(position);
+  const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+
+  // 3) Then we return an object with the data that we are interested in
+
+  // { returned position and address are the payloads of the fullfilled action state }
+  return { position, address };
+})
 
 const initialState = {
   username: "sam", 
+  status : "idle", 
+  position : {}, 
+  address : "", 
+  error : "", 
+
 }
 
 const userSlice = createSlice(
@@ -33,7 +53,18 @@ const userSlice = createSlice(
       updateName(state, action) {
         state.username = action.payload; 
       }
-    }
+    }, 
+    // specify the 3 action states 
+    extraReducers : (builder) => builder.addCase(fetchAddress.pending, (state , action) => state.status = 'loading').addCase(fetchAddress.fulfilled, (state , action) => {
+      state.status = "idle"; 
+      state.address = action.payload.address; 
+      state.position = action.payload; 
+
+
+    }).addCase(fetchAddress.rejected, (state, action) => {
+      state.status = "error"; 
+      state.error = action.error.message; 
+    })
   }
 )
 
